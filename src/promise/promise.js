01722f018e19e908fee0dcf3cae308d91b1211bb
Alex <akiosa88@gmail.com>
2025-04-28 18:28:56 +0400

    lesson: Promise

--- src/promise/promise.js
+++ src/promise/promise.js
@@ -5,48 +5,48 @@
 * */
 
 // Пример реализации промиса внутри какой-то библиотеки
-const server = {
-    getData() {
-        // В промис закидываем callback, который принимает две функции в качестве параметров. Логика этих функции инкапсулирована внутри промиса и направлена на работу с объектом, который он возвращает
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve('some error')
-            }, 2000)
-
-        })
-    }
-}
+// const server = {
+//     getData() {
+//         // В промис закидываем callback, который принимает две функции в качестве параметров. Логика этих функции инкапсулирована внутри промиса и направлена на работу с объектом, который он возвращает
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve('success')
+//             }, 2000)
+//
+//         })
+//     }
+// }
 
 // Использование промиса в реальном проекте. Просто вызываем метод, который вернет нам объект промис
-const promise = server.getData()
+// const promise = server.getData()
 
 // метод then() срабатывает, когда зарезолвится наш промис
-promise
-    .then((data) => {
-        console.log('Then1 ', data)
-        return 10
-    })
-    .then((data, dara) => {
-        console.log('Then2 ', data)
-        throw new Error("Oops error")
-    })
-    .then((data) => {
-        console.log('Then3 ', data)
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-    .finally(() => {
-        console.log('Finally ')
-    })
+// promise
+//     .then((data) => {
+//         console.log('Then1 ', data)
+//         return 10
+//     })
+//     .then((data, dara) => {
+//         console.log('Then2 ', data)
+//         throw new Error("Oops error")
+//     })
+//     .then((data) => {
+//         console.log('Then3 ', data)
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     })
+//     .finally(() => {
+//         console.log('Finally ')
+//     })
 
 // метод catсh() срабатывает, после срабатывания метода reject()
-promise.catch((error) => {
-    console.log('Catch', error)
-})
+// promise.catch((error) => {
+//     console.log('Catch', error)
+// })
 
 // Отрабатывает всегда. Используется, если нужна одна и та же логика, которая выполняется и в случае resolve(), и в случае reject()
-promise.finally('Finaly')
+// promise.finally('Finaly')
 
 
 // Пример собственной реализации промиса
@@ -94,10 +94,44 @@ const promise_2 = readFile('./index.js')
 // Написать функцию delay(ms), которая возвращает promise,
 // переходящий в состояние "resolved" через ms миллисекунд.
 
-function delay(time) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, time)
-    })
+// function delay(time) {
+//     return new Promise((resolve) => {
+//         setTimeout(resolve, time)
+//     })
+// }
+//
+// delay(1000).then(() => alert("Hello"))
+
+function go() {
+    // showCircle(150, 150, 100, div => {
+    //     div.classList.add('message-ball');
+    //     div.append("Привет, мир!");
+    // });
+
+    showCircle(150, 150, 100).then( div => {
+        div.classList.add('message-ball');
+        div.append("Привет, мир!");
+    });
 }
 
-delay(1000).then(()=> alert("Hello"))
+function showCircle (cx, cy, radius) {
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle';
+    document.body.append(div);
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            div.style.width = radius * 2 + 'px';
+            div.style.height = radius * 2 + 'px';
+
+            div.addEventListener('transitionend', function handler() {
+                div.removeEventListener('transitionend', handler);
+                resolve(div);
+            });
+        }, 0);
+    })
+}
